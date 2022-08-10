ce1be43f4b34fc6f953f4fa9f47d55767b4a1bb4
Mario Abdala <mario_abdala@hotmail.com>
2022-08-10 10:32:29 -0300

    Primer commit

---
+++ useTodo.js
@@ -0,0 +1,37 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer";
+
+export const useTodo = () => {
+    const initialState = [];
+    const init = () => {
+        return JSON.parse(localStorage.getItem('todos')) || [];
+    };
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos]);
+    const handleNewTodo = (todo) => {
+        const action = {
+            type: '[TODO] add todo',
+            payload: todo
+        };
+        dispatch(action);
+    };
+    const handleDeleteTodo = (todo) => {
+        const action = {
+            type: '[TODO] remove todo',
+            payload: todo
+        };
+        dispatch(action);
+    };
+    const handleToggleTodo = (todo) => {
+        const action = {
+            type: '[TODO] toggle todo',
+            payload: todo
+        };
+        dispatch(action);
+    };
+    const todosCount = todos.length;
+    const todosPendingCount = todos.filter(todo => !todo.done).length;
+    return {todos, todosCount, todosPendingCount, handleNewTodo, handleDeleteTodo, handleToggleTodo};
+};
